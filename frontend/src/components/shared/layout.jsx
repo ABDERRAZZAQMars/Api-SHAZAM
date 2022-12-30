3f0f255f3ee22d74c8d895f39bbb45750b4d5faa
abderrazzaq mars <abderrazzaqmars99@gmail.com>
2022-12-30 01:03:24 +0100

    footer

--- frontend/src/components/shared/layout.jsx
+++ frontend/src/components/shared/layout.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
-
-
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 function layout() {
   return (
-    <div className='flex flex-col'>
-        <Navbar />
-        
-        <div className="bg-gray-200 h-screen"><Outlet/></div>
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="bg-gray-200 h-screen">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default layout
+export default layout;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div className="bg-green">
-      Home Page
+    <div className="flex flex-col gap-2 p-10">
+      <h2 className="text-gray-900 font-bold flex justify-center">
+        TOP 200 MONDIAL
+      </h2>
+      <p className="text-gray-900 text-4xl font-bold flex justify-center">
+        Titres les plus découverts
+      </p>
+      <p className="text-gray-900 text-4xl font-bold flex justify-center">
+        dans le monde en ce moment
+      </p>
+      <p className="flex justify-center">
+        Découvrez les titres les plus shazamés dans le monde
+      </p>
     </div>
   );
 }
